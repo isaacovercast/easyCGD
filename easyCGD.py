c090ba9ad3259d7e86174c3ead8f6e1bfd90fe0b
Isaac Overcast <isaac.overcast@gmail.com>
2020-04-30 08:30:02 -0400

    updates for lots of functions including pi/dxy and plotting routines

--- easyCGD.py
+++ easyCGD.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-from collections import Counter
+from collections import Counter, OrderedDict
 from itertools import combinations
 from scipy.stats import entropy,linregress
 from scipy.spatial.distance import pdist, squareform
@@ -43,13 +43,26 @@ def hill_numbers(abunds, orders, granularity=None, do_negative=False):
 
 
 def pi(fasta_file, quiet=True):
+    ## This is multi-line style fasta, so we have to join the lines
     ## Calculate pi from a fasta file
     pi = 0
     len_seq = 0
     try:
         f = open(fasta_file).readlines()
+        f_dict = {}
+        name = ''
+        for line in f:
+            if ">" in line:
+                name = line.strip()
+                f_dict[name] = [""]
+            else:
+                f_dict[name] = f_dict[name] + list(line.strip())
+        dat = list(f_dict.values())
+
+        ## Old way that only does single line
         ## Get just the sequences
-        dat = [list(x.strip()) for x in f if ">" not in x]
+        #dat = [list(x.strip()) for x in f if ">" not in x]
+            
         len_seq = len(dat[0])
 
         ## Transpose, so now we have a list of lists of all bases at each
@@ -83,13 +96,37 @@ def pi(fasta_file, quiet=True):
     return pi
 
 
+    ## Calculate Dxy between 2 arrays of globally aligned sequences.
+    ## This function will ingore indels and N's.
+    def dxy(aseqs, bseqs):
+        Dxy = 0
+        for a in aseqs:
+            for b in bseqs:
+                ## Get counts of bases that differ between seqs
+                diffs = np.sum(~np.array(a == b, dtype=np.bool))
+                indela = a == "-"
+                indelb = b == "-"
+                ## Get counts of indels that are present in one seq and not the other
+                indels = np.sum(~np.array(indela == indelb))
+                na = a == "N"
+                nb = b == "N"
+                ## Get count of Ns that are present in one seq and not the other
+                ns = np.sum(~np.array(na == nb))
+                ## Get average number of differences per base, not counting indels and Ns
+                Dxy += (diffs - indels - ns)/len(a)
+        return Dxy/(len(aseqs)*len(bseqs))
+
+
 #######################################################
 ## Plotting functions
 #######################################################
 
 
-def plot_hill_numbers(in_df, order=5, do_negative=False, title="Hill numbers", quiet=True, axis_labels=True, ax=None, color=None):
+def plot_hill_numbers(in_df, order=5, do_negative=False, title="Hill numbers", quiet=True, label=None, axis_labels=True, ax=None, color=None):
     hill_results = {}
+    if isinstance(in_df, list):
+        print(label)
+        in_df = pd.DataFrame(in_df, columns=[label])
     for name, vals in in_df.items():
         hill_results[name] = hill_numbers(vals, orders=order, granularity=None, do_negative=do_negative)
     if not quiet: print(hill_results)
@@ -98,9 +135,9 @@ def plot_hill_numbers(in_df, order=5, do_negative=False, title="Hill numbers", q
     if do_negative:
         X = np.linspace(-order, order, num=order*2+2)
         ax.axvline(0, color="k", alpha=0.2, linewidth=3, linestyle="--")
-
     else:
         X = range(order+1)
+    ## Allow to just pass in a list
     for region, h in hill_results.items():
         ## If not specifying the color then we want to just use random different colors for each line
         if color:
@@ -119,11 +156,13 @@ def plot_hill_numbers(in_df, order=5, do_negative=False, title="Hill numbers", q
     return ax
 
 
-def plot_RACs(in_df, trim_zeros=True, title="Rank abundance", label=None, axis_labels=True, ax=None, color=None):
+def plot_RACs(in_df, trim_zeros=True, title="Rank abundance", plot_type="Abundance", label=None, axis_labels=True, ax=None, alpha=0.65, color=None, add_sim_label=None):
     if not ax:
         fig, ax = plt.subplots(figsize=(10, 10))
-    if label == None:
-       pass 
+    if not label is None:
+       add_sim_label = label
+    if isinstance(in_df, list):
+        in_df = pd.DataFrame(in_df)
     for c in in_df.columns:
         all_abunds = in_df[c].fillna(0).values
         if trim_zeros:
@@ -131,20 +170,26 @@ def plot_RACs(in_df, trim_zeros=True, title="Rank abundance", label=None, axis_l
         else:
             all_abunds = np.sort(all_abunds)[::-1]
         X = np.arange(0, len(all_abunds))
+        if add_sim_label:
+            c = add_sim_label
         if color:
-            ax.semilogy(X, all_abunds, label=c, linewidth=4, alpha=0.65, color=color)
+            ax.semilogy(X, all_abunds, label=c, linewidth=4, alpha=alpha, color=color)
         else:
-            ax.semilogy(X, all_abunds, label=c, linewidth=4, alpha=0.65)
+            ax.semilogy(X, all_abunds, label=c, linewidth=4, alpha=alpha)
     if axis_labels:
         plt.xlabel("Rank", fontsize=25)
-        plt.ylabel("log10(Abundance)", fontsize=25)
+        plt.ylabel("log10({})".format(plot_type), fontsize=25)
     plt.tick_params(axis='both', which='major', labelsize=15)
     plt.title(title, fontsize=30)
-    plt.legend(fontsize=15)
+
+    ## Remove duplicates in the case of plotting sim RACs
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = OrderedDict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys(), fontsize=15)
     return ax
 
 
-def plot_distances(abunds, labels=None, metric="braycurtis", title="distance"):
+def plot_distances(abunds, labels=None, metric="braycurtis", title=""):
     ## Private function for munging data into a square format
     def mksq(abunds, metric):
         dist = pdist(abunds, metric=metric)
@@ -173,20 +218,21 @@ def plot_distances(abunds, labels=None, metric="braycurtis", title="distance"):
     heatmap = ax.pcolor(sq1, cmap="magma")
     cbar = plt.colorbar(heatmap)
     # Set ticks in center of cells
-    _ = ax.set_xticks(np.arange(len(labels)) + 0.5, minor=False)
-    _ = ax.set_yticks(np.arange(len(labels)) + 0.5, minor=False)
+    ax.set_xticks(np.arange(len(labels)) + 0.5, minor=False)
+    ax.set_yticks(np.arange(len(labels)) + 0.5, minor=False)
 
-    _ = ax.set_xticklabels(labels, rotation=45, ha="right", rotation_mode="anchor")
-    _ = ax.set_yticklabels(labels)
-    ax.set_title("{} - {} Distance(s)".format(title, metric))
+    ax.set_xticklabels(labels, rotation=45, ha="right", rotation_mode="anchor")
+    ax.set_yticklabels(labels)
+    plt.tick_params(axis='both', which='major', labelsize=15)
+    plt.suptitle("{} {} distance(s)".format(title, metric), fontsize=30)
     return ax, sq1
 
 
 ## Plot correlation between abundances and genetic diversities. Input should be dataframes
 ## with index indicating OTU for both frames.
 def plot_abundance_diversity_correlation(a_df, g_df, ax=None, drop_zeros=False, color=None, log_transform=False,\
-                                        do_proportions=False, title="Abundance Genetic Diversity Correlation",\
-                                        quiet=True):
+                                        do_proportions=False, label=None, title="Abundance Genetic Diversity Correlation",\
+                                        make_square=False, legend=True, quiet=True):
     if ax == None:
         _, ax = plt.subplots(figsize=(10, 10))
     if isinstance(color, str):
@@ -194,25 +240,34 @@ def plot_abundance_diversity_correlation(a_df, g_df, ax=None, drop_zeros=False, 
     elif np.any(color):
         pass
     else:
-        color = np.random.rand(3,)
+        pass
+        #color = np.random.rand(3,)
+
+    ## TODO: This is hax
+    if label is None:
+        label = a_df.columns[0]
 
     ## if doing proportions scale all values to sum to 1
+    ## You don't want to do both proportional plotting and log transforming
+    ## of abundances so here we have a conditional to account for this.
+    xlab = "Abundance"
     if do_proportions:
         tmp_pis_df = g_df/g_df.sum(axis=0)
         tmp_abund_df = a_df/a_df.sum(axis=0)
+    elif log_transform:
+        tmp_pis_df = g_df
+        tmp_abund_df = np.log10(a_df)
+        xlab = "log10(Abundance)"
     else:
         tmp_pis_df = g_df
         tmp_abund_df = a_df
 
-    xlab = "Abundance"
-    if log_transform:
-        tmp_pis_df = g_df
-        tmp_abund_df = np.log10(a_df)
-        xlab = "log10(Abundance)"
 
     ## Get global max x and y values for each ABC across all regions
     xmax = tmp_abund_df.max().values[0]
     ymax = tmp_pis_df.max().values[0]
+    if make_square:
+        xmax = ymax = max([xmax, ymax])
     ax.set_xlim(0, xmax)
     ax.set_ylim(0, ymax)
 
@@ -229,19 +284,21 @@ def plot_abundance_diversity_correlation(a_df, g_df, ax=None, drop_zeros=False, 
 
     ## Draw the linear regression line
     linreg = linregress(tmp_df["abund"], tmp_df["genet"])
-    X = np.linspace(0, max([xmax, ymax]), 100)
+    X = np.linspace(0, 10, 100)
     Y = X*linreg.slope + linreg.intercept
-    ax.plot(X, Y, color=color, alpha=.5)
+    ax.plot(X, Y, color=color, alpha=.5, lw=3)
 
-    ax.scatter(tmp_df["abund"], tmp_df["genet"], label="{:.4f} - {}".format(linreg.rvalue**2, "MESS Simulations"), color=color)
+    ax.scatter(tmp_df["abund"], tmp_df["genet"], label="R^2={:.4f} - {}".format(linreg.rvalue**2, label), color=color)
 
     if not quiet: print(linreg)
     plt.xlabel(xlab, fontsize=25)
+    ## TODO: Get pi printing right here
     plt.ylabel("Genetic Diversity", fontsize=25)
     plt.tick_params(axis='both', which='major', labelsize=15)
-    plt.legend(loc="upper right")
-    plt.suptitle(title, fontsize=30)
-    return ax
+    if legend:
+        plt.legend(loc="upper right", fontsize=15)
+    plt.title(title, fontsize=30)
+    return ax, tmp_df
 
 #######################################################
 ## Loading functions
@@ -250,6 +307,8 @@ def plot_abundance_diversity_correlation(a_df, g_df, ax=None, drop_zeros=False, 
 def get_pis_from_fastas(fasta_dir, outfile=None, colname=None, quiet=True):
     try:
         files = glob.glob(os.path.join(fasta_dir, "*.fasta"))
+        if not files:
+            raise Exception("No fasta files in this directory: {}".format(files))
         if not colname:
             colname = [fasta_dir.strip("/").split("/")[-1]]
         if not isinstance(colname, list):
